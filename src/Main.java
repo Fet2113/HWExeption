5920fa36b8fdd8b34538def8a91830f7a4d7a455
Fet2113 <super.gjoll@yandex.ru>
2023-07-01 23:40:31 +0300

    ДЗ по Exeption

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        checkLoginAndPassword("Test", "1Test", "Test");
+    }
+
+    public static void checkLoginAndPassword(String login, String password, String confirmPassword) {
+
+        try {
+            checkLogin(login);
+            checkPassword(password, confirmPassword);
+        } catch (WrongLoginException | WrongPasswordException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("Конец");
+        }
+    }
+
+    // Проверяем login
+    private static void checkLogin(String login) throws WrongLoginException {
+        if (!login.matches("^[a-zA-Z0-9_]{1,20}$")) {
+            throw new WrongLoginException("Ошибка!Используются недопустимые для логина символы: " + login);
+        }
+    }
+    // Проверяем password
+
+    private static void checkPassword(String password, String confirmPassword) throws WrongPasswordException {
+        if (!password.matches("^[a-zA-Z0-9_]{1,20}$")) {
+            throw new WrongPasswordException("Ошибка!Используются недопустимые для пароля символы: " + password);
+        }
+        if (!password.equals(confirmPassword)) {
+            throw new WrongPasswordException("Пароли не совпадают");
+        }
+
+    }
+}
